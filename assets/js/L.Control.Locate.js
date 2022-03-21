c6ba42267f3b2b336e4ee4581c60fcf0bb278cea
leosanchez92 <nelsonsanchez.al@gmail.com>
2022-03-21 17:17:15 -0300

    BETA

--- assets/js/L.Control.Locate.js
+++ assets/js/L.Control.Locate.js
@@ -227,8 +227,8 @@ You can find the project at: https://github.com/domoritz/leaflet-locatecontrol
             /** Accuracy circle style properties. NOTE these styles should match the css animations styles */
             circleStyle: {
                 className:   'leaflet-control-locate-circle',
-                color:       '#D36143',
-                fillColor:   '#D36143',
+                color:       '#136AEC',
+                fillColor:   '#136AEC',
                 fillOpacity: 0.15,
                 weight:      0
             },
@@ -236,7 +236,7 @@ You can find the project at: https://github.com/domoritz/leaflet-locatecontrol
             markerStyle: {
                 className:   'leaflet-control-locate-marker',
                 color:       '#fff',
-                fillColor:   '#D36143',
+                fillColor:   '#2A93EE',
                 fillOpacity: 1,
                 weight:      3,
                 opacity:     1,
